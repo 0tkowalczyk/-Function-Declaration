9aa07c58229419da64a58c32dd5498c2da050f89
0tkowalczyk <42749742+0tkowalczyk@users.noreply.github.com>
2018-09-26 13:58:46 -0500

    Add files via upload

--- f1.js
+++ f1.js
@@ -9,8 +9,8 @@
 
 // (1) First Task
 // Declare a function named double() which outputs a number double the input number.
-function double(number){
-    return number*2
+function double (x){
+    return x*2
 }
 
 
@@ -20,8 +20,8 @@ function double(number){
 
 // (2) Second Task
 // Declare a function named plusplus() which outputs a number one higher than the input number.
-function plusplus(number){
-    return number+1
+function plusplus(x){
+    return x+1
 }
 
 
@@ -32,8 +32,8 @@ function plusplus(number){
 
 // (3) Third Task
 // Declare a function named oppRecip() which outputs the opposite reciprocal of the input number.
-function oppRecip (number){
-    return number - 2 * number;
+function oppRecip (x){
+    return -1/x;
 }
 
 
@@ -66,7 +66,7 @@ function circleArea(radius){
 // (6) Sixth Task
 // Declare a function named boxVolume() which has three inputs: length, width, and depth.
 // The function outputs the volume of the box;
-function boxvolume(lenght, width, depth){
+function boxVolume(lenght, width, depth){
     return lenght * width * depth
 }
 
@@ -82,8 +82,8 @@ function boxvolume(lenght, width, depth){
 // (7) Seventh Task
 // Declare a function named thisIsMadness() which accepts one string input named city.
 // The function should output the string: "No! This is <city>!!"
-function thisIsMadness("city"){
-    return "No! This is" + "city"
+function thisIsMadness(city){
+    return "No! This is " + city
 }
 
 
@@ -94,9 +94,9 @@ function thisIsMadness("city"){
 // (8) Eigth Task
 // Declare a function named yo() which accepts one string input named fam.
 // The function should output the string: "yo <fam>!  What's good <fam>??"
-
-
-
+function yo(fam){
+    return "yo " + fam + " What's good " + fam + "??";
+}
 
 
 
@@ -104,7 +104,9 @@ function thisIsMadness("city"){
 // (9) Ninth Task
 // Declare a function named beefDay() which has no inputs.
 // This function should output a long string, IN ALL CAPS, of "BEEF DAY" with lots of extra E's, F's, A's, and Y's
-
+function beefDay(){
+    return "BEEEEFFFF DAAAAYYYY";
+}
 
 
 
@@ -129,3 +131,24 @@ function thisIsMadness("city"){
 
 
 /**************************** END OF FILE *******************************/
+//Declaration of main function
+
+function main(){
+    let x = 5;
+    console.log(`For the next few examples let x = ${x}`);
+    console.log(`1. double the number = ${double(x)}`);
+    console.log(`2. adding one to the number = ${plusplus(x)}`);
+    console.log(`3. the opposite reciprocal of ${x} is ${oppRecip(x)}`);
+    console.log(`4. the pythagorean theorem does 5 = ${pythagorean(3,4)}?`);
+    console.log(`5. the circle with radius ${x} has area = ${circleArea(x)}`);
+    console.log(`6. the volume of a ${x}, ${2*x}, ${3*x} box has volume = ${boxVolume(x,2*x,3*x)}`);
+    console.log(`7. ${thisIsMadness("SPARTA")}`);
+    console.log(`8. ${yo("dude")}`);
+    console.log(`9. ${beefDay()}`);
+    console.log(`10. first test: true == ${xor(true,false)} == ${xor(false,true)}`);
+    console.log(`10. second test: false == ${xor(true,true)} == ${xor(false,false)}`);
+  
+  }
+  
+  //execution of main function
+  main();
